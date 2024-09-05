814588f961fc07539c617d98d068a55e23869dbe
Junseo Yun <yunjunseo@naver.com>
2024-09-06 05:13:03 +0900

    Create Auth

---
+++ Muscle/src/main/java/Muscle/auth/controller/AuthController.java
@@ -0,0 +1,123 @@
+package Muscle.auth.controller;
+
+import Muscle.auth.dto.RequestAuth;
+import Muscle.auth.dto.ResponseAuth;
+import Muscle.auth.security.JwtAuthTokenProvider;
+import Muscle.auth.service.AuthService;
+import Muscle.auth.service.EmailService;
+import Muscle.common.dto.ResponseDto;
+import Muscle.common.dto.ResponseMessage;
+import Muscle.common.exception.error.LoginFailedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    private final JwtAuthTokenProvider jwtAuthTokenProvider;
+    private final EmailService emailService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseMessage> registerUser(@Valid @RequestBody RequestAuth.RegisterUserDto registerUserDto) {
+        authService.registerUser(registerUserDto);
+        ResponseMessage responseMessage = ResponseMessage.builder()
+                .message("User registered successfully.")
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseMessage> loginUser(@Valid @RequestBody RequestAuth.LoginUserRqDto loginUserRqDto) {
+        ResponseAuth.LoginUserRsDto response = authService.loginUser(loginUserRqDto).orElseThrow(() -> new LoginFailedException());
+        ResponseMessage responseMessage = ResponseMessage.builder()
+                .message("User logged in successfully.")
+                .data(response)
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
+    }
+
+    @PostMapping ("/sendEmail")
+    public String sendEmail(@RequestBody @Valid RequestAuth.SendEmailDto sendEmailDto){
+        System.out.println("이메일 인증 이메일 :"+sendEmailDto.getEmail());
+        return emailService.writeEmail(sendEmailDto.getEmail());
+    }
+
+    @PostMapping("/verifyEmail")
+    public ResponseEntity<ResponseMessage> verifyEmail(@RequestBody @Valid RequestAuth.VerifyEmailDto verifyEmailDto){
+        boolean Checked=emailService.verifyEmail(verifyEmailDto.getEmail(),verifyEmailDto.getVerificationCode());
+        if(Checked){
+            ResponseMessage responseMessage = ResponseMessage.builder()
+                    .message("Email verified successfully.")
+                    .build();
+            return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
+        }
+        else{
+            throw new NullPointerException("Verification failed.");
+        }
+    }
+
+    @PostMapping("/getTempToken")
+    public ResponseEntity<ResponseMessage> getTempToken(@RequestBody @Valid RequestAuth.VerifyEmailDto verifyEmailDto){
+        String tempToken = authService.getTempToken(verifyEmailDto.getEmail(), verifyEmailDto.getVerificationCode());
+        ResponseMessage responseMessage = ResponseMessage.builder()
+                .message("TempToken issued successfully.")
+                .data(tempToken)
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
+    }
+
+//    @PostMapping("/uploadImg")
+//    public ResponseEntity<ResponseDto> uploadUserImg(@RequestPart(value = "file", required = false) MultipartFile file, HttpServletRequest request){
+//        Optional<String> token = jwtAuthTokenProvider.getAuthToken(request);
+//        String url = authService.uploadImg(file, token);
+//        ResponseDto responseDto = ResponseDto.builder()
+//                .message("Image uploaded successfully.")
+//                .data(url)
+//                .build();
+//        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+//    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseMessage> updateUser(HttpServletRequest request, @Valid @RequestBody RequestAuth.UpdateUserDto updateUserDto) {
+        Optional<String> token = jwtAuthTokenProvider.getAuthToken(request);
+        authService.updateUser(token, updateUserDto);
+        ResponseMessage responseMessage = ResponseMessage.builder()
+                .message("User information updated successfully.")
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
+    }
+
+    @PutMapping("/changePassword")
+    public ResponseEntity<ResponseMessage> changePassword(HttpServletRequest request, @Valid @RequestBody RequestAuth.ChangePasswordDto changePasswordDto){
+        Optional<String> token = jwtAuthTokenProvider.getAuthToken(request);
+        authService.changePassword(token, changePasswordDto.getPassword());
+        ResponseMessage responseMessage = ResponseMessage.builder()
+                .message("Password changed successfully.")
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<ResponseMessage> getUser(HttpServletRequest request) {
+        Optional<String> token = jwtAuthTokenProvider.getAuthToken(request);
+        ResponseAuth.GetUserDto response = authService.getUser(token);
+        ResponseMessage responseMessage = ResponseMessage.builder()
+                .message("User information retrieved successfully.")
+                .data(response)
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
+    }
+
+}
---
+++ Muscle/src/main/java/Muscle/auth/dto/ResponseAuth.java
@@ -0,0 +1,38 @@
+package Muscle.auth.dto;
+
+
+import Muscle.auth.entity.Auth;
+import lombok.Builder;
+import lombok.Data;
+
+public class ResponseAuth {
+    @Data
+    @Builder
+    public static class LoginUserRsDto{
+        private String accessToken;
+
+        public static LoginUserRsDto toDto(String accessToken){
+            return LoginUserRsDto.builder()
+                    .accessToken(accessToken)
+                    .build();
+        }
+    }
+
+    @Data
+    @Builder
+    public static class GetUserDto{
+        private String email;
+        private String name;
+        private String nickName;
+        private String userImg;
+
+        public static GetUserDto toDto(Auth user){
+            return GetUserDto.builder()
+                    .email(user.getEmail())
+                    .name(user.getName())
+                    .nickName(user.getNickName())
+                    .userImg(user.getUserImg())
+                    .build();
+        }
+    }
+}
