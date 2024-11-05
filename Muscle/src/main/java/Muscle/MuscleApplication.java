668cc6ab430997e561c2be4b54faa8cad37729c3
Junseo Yun <yunjunseo@naver.com>
2024-11-06 05:51:18 +0900

    Set time zone

--- Muscle/src/main/java/Muscle/MuscleApplication.java
+++ Muscle/src/main/java/Muscle/MuscleApplication.java
@@ -1,11 +1,17 @@
 package Muscle;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MuscleApplication {
-
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MuscleApplication.class, args);
 	}
